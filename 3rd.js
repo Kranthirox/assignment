7c359921f8ac068c60d495ab1a1fad8d8238780b
Kranthirox <kranthiprasadnaw@gmail.com>
2022-03-23 15:12:12 +0530

    remove and add new files

--- 3rd.js
+++
@@ -1,5 +0,0 @@
-var myStr = 0;
-var stateM =  " I'm learning coding";
-
-var myStr = stateM.length
-console.log(myStr)
---
+++ COUNTER.js
@@ -0,0 +1,27 @@
+let counter = document.getElementById("counter");
+let count = 0;
+function increment() {
+  count = count + 1;
+  counter.innerHTML = count;
+  checkForColor();
+}
+function decrement() {
+  count = count - 1;
+  counter.innerHTML = count;
+  checkForColor();
+}
+function zero() {
+  count = 0;
+  counter.innerHTML = count;
+  checkForColor();
+}
+
+function checkForColor() {
+  if (count < 0) {
+    document.getElementById("counter").style.color = "red";
+  } else if (count > 0) {
+    document.getElementById("counter").style.color = "green";
+  } else {
+    document.getElementById("counter").style.color = "black";
+  }
+}
